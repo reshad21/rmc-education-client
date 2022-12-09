673b0335ba565f59d040a5ba4852b2c4a3d613c0
reshad21 <miakala7@gmail.com>
2022-12-09 11:20:03 +0600

    blog and contact page create

--- src/Pages/Shared/Navber/Navber.jsx
+++ src/Pages/Shared/Navber/Navber.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Navber = () => {
     const menuItems = <>
-        <li><Link>Home</Link></li>
-        <li><Link>Contact Us</Link></li>
-        <li><Link>Blog</Link></li>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/contact'>Contact Us</Link></li>
+        <li><Link to='/blog'>Blog</Link></li>
     </>
     return (
         <div>
--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Blog from "../Pages/Blog/Blog";
+import Contact from "../Pages/Contact/Contact";
 import Home from "../Pages/Home/Home/Home";
 const router = createBrowserRouter([
     {
@@ -9,6 +11,14 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/contact',
+                element: <Contact></Contact>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
             }
         ]
     }
